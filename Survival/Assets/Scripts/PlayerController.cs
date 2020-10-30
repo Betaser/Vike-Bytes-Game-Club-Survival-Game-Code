2230125292a46ca931663da4220e04b30293e76e
Quentin-Ludet <quentinludet@gmail.com>
2020-10-30 16:05:45 -0400

    Its a moving circle

--- Survival/Assets/Scripts/PlayerController.cs
+++ Survival/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
     }
 }
