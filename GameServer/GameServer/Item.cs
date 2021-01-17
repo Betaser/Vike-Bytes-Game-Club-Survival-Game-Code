3e5e857aa18809994c08540d829b235d550d3e99
jacksherl <65086686+jacksherl@users.noreply.github.com>
2021-01-16 20:41:16 -0500

    item system

---
+++ GameServer/GameServer/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Item
+{
+
+	protected Item[] requirements;
+	public String name;
+	public Item(String name)
+	{
+		requirements = null;
+		this.name = name;
+	}
+	public Item (Item[] requirements, String name)
+    {
+		this.requirements = requirements;
+		this.name = name;
+    }
+}
---
+++ GameServer/GameServer/Spear.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Spear : Item
+{
+	public short damage;
+	public Spear() : base(new Item[] { new Item("Wood"), new Item("Stone") }, "Spear")
+	{
+		this.damage = 50;
+	}
+}
