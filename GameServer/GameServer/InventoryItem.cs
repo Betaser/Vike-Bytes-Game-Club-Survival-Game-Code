c12c2726d30355c3828589c02764bc20d576e3eb
jacksherl <65086686+jacksherl@users.noreply.github.com>
2021-06-04 16:24:39 -0400

    add inventory items

---
+++ GameServer/GameServer/InventoryItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+using System.Collections.Generic;
+
+namespace GameServer {
+	public class InventoryItem
+	{
+		public static int allId;
+		public int id;
+		public string name;
+		public Dictionary<string, int> ingredients;
+
+		public static List<InventoryItem> items = new List<InventoryItem>();
+
+		public InventoryItem(string name, Dictionary<string, int> ingredients)
+        {
+			this.name = name;
+			this.ingredients = ingredients;
+			this.id = allId;
+			allId++;
+        }
+
+		// ONLY RUN ONCE!!!
+		public void initializeItems ()
+        {
+			Dictionary<string, int> ingredients = new Dictionary<string, int>();
+			// add all items
+			ingredients["wood"] = 5;
+			items.Add(new InventoryItem("Wood Armor", ingredients));
+			ingredients.Clear();
+
+			ingredients["iron"] = 1;
+			ingredients["wood"] = 2;
+			items.Add(new InventoryItem("Iron Spear", ingredients));
+			ingredients.Clear();
+		}
+
+		//private Dictionary<string, int> getIngredientsToInitializeItem (str)
+        //{
+
+        //}
+	}
+}
